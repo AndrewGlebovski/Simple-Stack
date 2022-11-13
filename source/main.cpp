3b2963f54775e084ac02de2e254c5e4d71c23e86
Glebovski <andrewglebovski@gmail.com>
2022-11-13 17:51:43 +0300

    Add implementation

--- source/main.cpp
+++ source/main.cpp
@@ -3,6 +3,22 @@
 
 
 int main() {
+    Stack stack = {};
+
+    stack_constructor(&stack, 16);
+
+    for(int i = 0; i < 16; i++)
+        stack_push(&stack, i);
+
+    for(int i = 0; i < 14; i++) {
+        int value;
+        stack_pop(&stack, &value);
+    }
+
+    stack_dump(&stack, 0, stdout);
+
+    stack_destructor(&stack);
+
     printf("Stack!\n");
 
     return 0;
--- source/stack.hpp
+++ source/stack.hpp
@@ -15,6 +15,31 @@ typedef struct {
 #define OBJECT_TO_STR "%i"
 
 
+/// Exit codes for stack functions
+enum EXIT_CODES {
+    INVALID_DATA     =  1,        ///< Invalid stack data points
+    INVALID_SIZE     =  2,        ///< Size is larger than capacity or negative
+    INVALID_CAPACITY =  3,        ///< Capacity is larger than type's maximum or negative
+    UNEXP_POISON_VAL =  4,        ///< Unexpected poison value before size
+    UNEXP_NORMAL_VAL =  5,        ///< Unexpected normal value after size
+    INVALID_ARGUMENT =  6,        ///< Invalid argument given to the function
+    EMPTY_STACK      =  7,        ///< No elements to pop
+    ALLOCATE_FAIL    =  8,        ///< Memory allocate return NULL
+};
+
+
+/**
+ * \brief Prints stack content
+ * \param [in] stack Stack to print
+ * \param [in] error This error code will printed
+*/
+#define STACK_DUMP(stack, error)                                                        \
+do {                                                                                    \
+    fprintf(stdout, "%s at %s(%d)\n", __PRETTY_FUNCTION__, __FILE__, __LINE__);         \
+    stack_dump(stack, error, stdout);                                                   \
+} while(0)
+
+
 /**
  * \brief Constructs the stack
  * \param [in] stack    This stack will be filled
